06105a79e526cce9ae12af7798f90294315c11c9
Parth Sishodia <91628095+parthsishodia@users.noreply.github.com>
2024-03-22 21:48:14 +0530

    Time: 1 ms (100.00%), Space: 45.3 MB (15.62%) - LeetHub

---
+++ 2108-find-first-palindromic-string-in-the-array/2108-find-first-palindromic-string-in-the-array.java
@@ -0,0 +1,33 @@
+class Solution {
+    public String firstPalindrome(String[] words) {
+        for(int i=0;i<words.length;i++)
+        {
+            if(isPalidrome(words[i]))
+            {
+                return words[i];
+            }
+           
+        }
+         return "";
+        
+    }
+    
+    public static boolean isPalidrome(String str)
+    {
+        int left=0;
+        int right=str.length()-1;
+        while(left<right)
+        {
+        if(str.charAt(left)!=str.charAt(right))
+        {
+            return false;
+        }
+        left++;
+        right--;
+        
+        
+    }
+    return true;
+    
+}
+}
